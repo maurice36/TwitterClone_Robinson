68252e38f09a4a546ee4e3ef525beef6a57ce022
maurice36 <nmric3@gmail.com>
2022-12-27 15:50:53 -0700

    dfghjk

---
+++ static/js/posts.js
@@ -0,0 +1,10 @@
+$ (function() {
+    // Executed when js-menu-icon js clicked
+    $('.js-menu-icon').click(function(){
+    // $(this) : self element, namely div.js-menu-icon
+    // next()  : Next to div.js-menu-icon,namely div.menu
+
+       $(this).next().toggle();
+        
+    })
+})
